bc6ae57dee3a1339d3974a78482ef86b9368efcc
Alison-Weatherston <alison.weatherston@justice.gov.uk>
2026-03-13 12:49:10 +0000

    changed the text to Accredited Programmes and changed the path after login to route

--- steps/auth/login.ts
+++ steps/auth/login.ts
@@ -2,12 +2,14 @@ import { expect, type Page } from '@playwright/test'
 import { getConfig } from '../../appConfig'
 
 const appConfig = getConfig()
+const manageAndDeliverRootUrl = new URL('/', appConfig.MANAGE_AND_DELIVER_URL).toString()
 
 export const manageAndDeliverCommunityLogin = async (page: Page) => {
   await page.goto(appConfig.MANAGE_AND_DELIVER_URL)
   await page.getByLabel('Username').fill(appConfig.HMPPS_COMMUNITY_AUTH_USERNAME)
   await page.getByLabel('Password').fill(appConfig.HMPPS_COMMUNITY_AUTH_PASSWORD)
   await page.locator('#submit', { hasText: 'Sign in' }).click()
-  await expect(page.getByText('Building Choices:')).toBeVisible()
+  await expect(page).toHaveURL(manageAndDeliverRootUrl)
+  await expect(page.getByText('Accredited Programmes')).toBeVisible()
 }
 
