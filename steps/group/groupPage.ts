934fd2fc608d240263086084c7193b269b091b51
Alison-Weatherston <alison.weatherston@justice.gov.uk>
2026-07-09 08:45:50 +0100

    pdu amended

--- steps/group/groupPage.ts
+++ steps/group/groupPage.ts
@@ -108,8 +108,8 @@ export const selectAndSubmitSexRadioOption = async (
 };
 
 export const enterAndSubmitPdu = async (page: Page, pdu: string) => {
-  await page.locator("#create-group-pdu").fill(pdu);
-  await page.getByRole("option", { name: pdu }).first().click();
+  await page.locator("input#create-group-pdu").fill(pdu);
+  await page.locator("#create-group-pdu__listbox li").filter({ hasText: pdu }).first().click();
   await page.getByRole("button", { name: "Continue" }).click();
   await expect(page).toHaveURL(/.*\/group-delivery-location$/);
 };
